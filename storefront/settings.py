f9ca958db58fc0a9beab0603280247e536a6925f
Ted S <tsengretsi2@gmail.com>
2026-02-25 23:12:08 +0000

    Connected to DB

--- storefront/settings.py
+++ storefront/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'USER': 'root',
+        'PASSWORD': 'p@sSw0rD2*1',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
