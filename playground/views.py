df6fc2aa80d1b6a78db246873f7913aa5695b0c3
Ted S <tsengretsi2@gmail.com>
2026-03-04 21:32:53 +0000

    Edited hello.html and views.py

--- playground/views.py
+++ playground/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
-# # say "hello"
-# def say_hello(request):
-#     # return a simple http response
-#     return HttpResponse("Hello World")
-
-def calculate():
-    x = 1
-    y = 2
-    return x  
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Customer, Product, Collection, Order, OrderItem
 
 # using the render function to render a template
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name':'Ted'})
+
+    # keyword=value
+    # queryset = Product.objects.filter(unit_price_gt=20)
+
+    # all products that contain the word 'coffee' in their title
+    # queryset = Product.objects.filter(title__icontains='coffee')
+
+    # all products that were updated in 2021
+    # queryset = Product.objects.filter(last_update__year=2021)
+
+    # 1. customers with .com accounts
+    # queryset = Customer.objects.filter(email__icontains='.com')
+
+    # 2. collections that don't have a featured product
+    # queryset = Collection.objects.filter(featured_product__isnull=True)
+
+    # 3. Products with low inventory (less than 10)
+    # queryset = Product.objects.filter(inventory__lt=10)
+
+    # 4. Orders placed by customers with id = 1
+    # queryset = Order.objects.filter(customer__id=1)
+
+    # 5. Order items for products in colleciton 3
+    # queryset = OrderItem.objects.filter(product__collection__id=3)
+
+    # exists = Product.objects.filter(pk=0).exists()
+    
+    # query_set.filter().filter().order_by()
+
+    # for product in query_set:
+    #     print(product)
+
+    # query_set[0:5] 
+
+    return render(request, 'hello.html', {'name':'Ted', 'products': list(queryset)})
 
 
