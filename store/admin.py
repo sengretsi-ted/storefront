7fb7098662635619aceec2a047b45584fbcc74b8
Ted S <tsengretsi2@gmail.com>
2026-03-15 23:33:43 +0000

    Adding Computed Columns and Selected Related Objects

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,25 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','unit_price']
+    list_display = ['title','unit_price','inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+# Setup Orders and Customers in the admin site
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
