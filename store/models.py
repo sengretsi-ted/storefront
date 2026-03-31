22d2885b8f68712f534f9d8e631145b4043b6fa6
Ted S <tsengretsi2@gmail.com>
2026-03-31 00:41:25 +0000

    Deleted objects

--- store/models.py
+++ store/models.py
@@ -80,7 +80,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT) #foreign key to Order, with cascade delete
-    product = models.ForeignKey(Product, on_delete=models.PROTECT) #foreign key to Product, with cascade delete
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems') #foreign key to Product, with cascade delete
     quantity = models.PositiveSmallIntegerField() #positive integer
     unit_price = models.DecimalField(max_digits=6, decimal_places=2) #decimal(6,2)
     
--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.db.models import ProtectedError
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
+from .models import Product, OrderItem
 from .serializer import ProductSerializer
 
 
@@ -22,7 +23,7 @@ def product_list(request):
               
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     if request.method == 'GET':
@@ -33,6 +34,12 @@ def product_detail(request, id):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitems.count() > 0:
+            return Response({'error': 'Product cannot be deleted because it is associated with existing orders.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
  
 @api_view()
 def collection_detail(request, pk):
